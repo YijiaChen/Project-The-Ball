fdc16735c82ce1a832ca39f95cde5d6315e696b4
DESKTOP-A30FH77\Yijia The Potato <whateverchen@yahoo.com>
2017-11-26 20:22:02 -0500

    Kinda working-ish clock tree...

--- Assets/ClockPop.cs
+++ Assets/ClockPop.cs
@@ -3,28 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClockPop : MonoBehaviour {
-    public GameObject Blob1;
-    public GameObject Blob2;
-    public GameObject Blob3;
-    public GameObject Blob4;
-    public GameObject Blob5;
-    public GameObject Blob6;
-
-    [Header("bushes")]
-
-    public GameObject bush1;
-    public GameObject bush2;
-    public GameObject bush3;
-    public GameObject bush4;
-    public GameObject bush5;
-    
-  
-
+    public GameObject blob;
+    [Header("bush")]
+    public GameObject bush;
     private float transtime = 80;
     private bool triggered = false;
+    private float blobtime = 40;
+    private bool everythingdone = false;
     // Use this for initialization
-    void Start() {  
-
+    void Start() {
+        bush.transform.localScale = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
@@ -32,27 +20,22 @@ public class ClockPop : MonoBehaviour {
         if (triggered == true&&transtime>=0)
         {
             transtime -= 1;
-            Blob1.transform.localScale += new Vector3(-0.002f, -0.002f, -0.002f);
-            Blob2.transform.localScale += new Vector3(-0.002f, -0.002f, -0.002f);
-            Blob3.transform.localScale += new Vector3(-0.002f, -0.002f, -0.002f);
-            Blob4.transform.localScale += new Vector3(-0.001f, -0.001f, -0.001f);
-            Blob5.transform.localScale += new Vector3(-0.002f, -0.002f, -0.002f);
-            Blob6.transform.localScale += new Vector3(-0.002f, -0.002f, -0.002f);
+            blob.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+            
         }
-       if(triggered==true && transtime < 0)
+       if(triggered==true && transtime < 0 && blobtime>=0)
         {
-            Destroy(Blob1);
-            Destroy(Blob2);
-            Destroy(Blob3);
-            Destroy(Blob4);
-            Destroy(Blob5);
-            Destroy(Blob6);
-            bush1.SetActive(true);
-            bush2.SetActive(true);
-            bush3.SetActive(true);
-            bush4.SetActive(true);
-            bush5.SetActive(true);
+            Destroy(blob);
+            bush.SetActive(true);
+            bush.transform.localScale += new Vector3(0.03f, 0.03f, 0.03f);
+            blobtime -= 1;
+
         }
+       if (blobtime < 0 && everythingdone==false)
+        {
+            bush.transform.localScale = new Vector3(1, 1, 1);
+            everythingdone = true;
+        }   
     }
 
     private void OnTriggerEnter(Collider collision) {
---
+++ Assets/Scripts/Clock1.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System;
+using UnityEngine;
+
+public class Clock1 : MonoBehaviour {
+
+    //float degreePerHour = 30f;
+    //float degreePerMinute = 6f;
+    float degreePerSecond = 0f;
+    float speed = 0;
+
+    public Transform secondsTransform;
+
+    private void Start() {
+        speed = Random.Range(0.1f, 6.0f);
+    }
+    //public bool continuous;
+
+    void Update() {
+        degreePerSecond += speed;
+        secondsTransform.localRotation = Quaternion.Euler(0f, degreePerSecond, 0f);
+    }
+}
+        /*if (continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+
+    void UpdateDiscrete ()
+    {
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation = 
+            Quaternion.Euler(0f, DateTime.Now.Hour * degreePerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, DateTime.Now.Minute * degreePerMinute, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, DateTime.Now.Second * degreePerSecond, 0f);
+    }
+
+    void UpdateContinuous ()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, (float) time.TotalHours * degreePerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, (float) time.TotalMinutes * degreePerMinute, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, (float) time.TotalSeconds * degreePerSecond, 0f);
+    }
+    
+}*/
