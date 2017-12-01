963c203279907f67b782388cced86e713b0aed2d
DESKTOP-A30FH77\Yijia The Potato <whateverchen@yahoo.com>
2017-12-01 15:13:31 -0500

    Ending

---
+++ Assets/Scripts/SceneController1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController1: MonoBehaviour
+{
+    GameObject audi;
+    public float jumpback;
+    private bool end = false;
+    private void Start()
+    {
+        audi = GameObject.Find("AudioManager");
+    }
+    private void Update()
+    {
+        if (end == true)
+        {
+            jumpback -= Time.deltaTime;
+            if (jumpback < 0)
+            {
+               SceneManager.LoadScene(0, LoadSceneMode.Single);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        
+        if (collision.gameObject.tag == "Player")
+        {
+            if (end == false)
+            {
+               audi.SendMessage("gateOpen");
+               end = true;
+            }
+           
+           
+            //Debug.Log("Restarting Game");
+
+        }
+    }
+}
