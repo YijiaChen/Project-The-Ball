12b8c4dc06b5337de839d1dac1a4abc92762a46f
Unknown <poe_s@hotmail.com>
2017-12-02 00:02:18 -0500

    Bunny idle+jump and script

---
+++ Assets/Scripts/BunnyAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyAnimation : MonoBehaviour {
+
+    public string movementAnimation;
+    Animator anim;
+
+
+	void Start () {
+        anim = GetComponent<Animator>();
+        float randomStart = Random.Range(0f, anim.GetCurrentAnimatorStateInfo(0).length);
+        anim.Play(movementAnimation, 0, randomStart);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
